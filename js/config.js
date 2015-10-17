19c6ab25e9d4c8fd98ad5e20014a5694786ebcab
Ubuntu Machine <ubuntu@mail.com>
2015-10-17 14:15:17 +0200

    added content

---
+++ js/config.js
@@ -0,0 +1,15 @@
+configEasyDriveCms = {
+    author: 'React Drive CMS',
+    /**
+     * Add the Id for the Google Sheets Dashboard file
+     */
+    dashboardId: '1-on_GfmvaEcOk7HcWfKb8B6KFRv166RkLN2YmDEtDn4',
+    /**
+     * Add the Url Id for the Google Web App Script to forward email from the Contact Form
+     */
+    sendMailUrlId: 'AKfycbyL4vW1UWs4mskuDjLoLmf1Hjan1rTLEca6i2Hi2H_4CtKUN84d',
+    /**
+     * Add your Disqus shortname
+     */
+    shortname: 'easydrivecms'
+};
